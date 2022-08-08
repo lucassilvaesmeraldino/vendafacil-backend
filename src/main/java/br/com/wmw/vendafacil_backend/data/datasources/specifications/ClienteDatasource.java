4794922bc489b33a6bae6117f4833fd753d29571
Lucas Silva Esmeraldino <lucas.ssilvaesmeraldino@gmail.com>
2022-08-08 14:01:32 -0300

    Commit de melhoria: alteração na estrutura do projeto

--- src/main/java/br/com/wmw/vendafacil_backend/data/datasources/specifications/ClienteDatasource.java
+++ src/main/java/br/com/wmw/vendafacil_backend/data/datasources/specifications/ClienteDatasource.java
@@ -1,11 +1,14 @@
-package br.com.wmw.vendafacil_backend.data.datasources;
+package br.com.wmw.vendafacil_backend.data.datasources.specifications;
 
 import java.util.List;
+import java.util.Optional;
 
 import br.com.wmw.vendafacil_backend.data.models.cliente.ClienteModel;
 
 public interface ClienteDatasource {
 
 	List<ClienteModel> getClientes();
+
+	Optional<ClienteModel> getClienteByCodigo(Long codigo);
 
 }
